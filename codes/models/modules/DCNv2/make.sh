2ff73333e8583724a61ee70a7ad511d7cda26089
XintaoWang <wxt1994@126.com>
2019-05-28 11:21:26 +0800

    add DCNv2

---
+++ codes/models/modules/DCNv2/make.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# You may need to modify the following paths before compiling.
+
+# CUDA_HOME=/usr/local/cuda-10.0 \
+# CUDNN_INCLUDE_DIR=/usr/local/cuda-10.0/include \
+# CUDNN_LIB_DIR=/usr/local/cuda-10.0/lib64 \
+python setup.py build develop
