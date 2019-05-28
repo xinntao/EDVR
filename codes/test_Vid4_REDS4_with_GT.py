aa7dcf99682afc46533bcc562adbf71c3236d5e8
XintaoWang <wxt1994@126.com>
2019-05-28 21:44:14 +0800

    minor updates

--- codes/test_Vid4_REDS4_with_GT.py
+++ codes/test_Vid4_REDS4_with_GT.py
@@ -169,10 +169,10 @@ def main():
         #### read GT images
         img_GT_l = []
         if data_mode == 'Vid4':
-            sub_folder_GT = sub_folder.replace('/BIx4/', '/GT/')
+            sub_folder_GT = osp.join(sub_folder.replace('/BIx4/', '/GT/'), '*')
         else:
-            sub_folder_GT = sub_folder.replace('/{}/'.format(data_mode), '/GT/')
-        for img_GT_path in sorted(glob.glob(sub_folder_GT + '/*')):
+            sub_folder_GT = osp.join(sub_folder.replace('/{}/'.format(data_mode), '/GT/'), '*')
+        for img_GT_path in sorted(glob.glob(sub_folder_GT)):
             img_GT_l.append(read_image(img_GT_path))
 
         avg_psnr, avg_psnr_border, avg_psnr_center = 0, 0, 0
