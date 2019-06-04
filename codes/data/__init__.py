4139f76c0a41eaeabc54bbd889a3966319df484e
XintaoWang <wxt1994@126.com>
2019-06-04 17:06:05 +0800

    add data/__init__

---
+++ codes/data/__init__.py
@@ -0,0 +1,45 @@
+'''create dataset and dataloader'''
+import logging
+import torch
+import torch.utils.data
+
+
+def create_dataloader(dataset, dataset_opt, opt, sampler):
+    phase = dataset_opt['phase']
+    if phase == 'train':
+        if opt['dist']:
+            world_size = torch.distributed.get_world_size()
+            rank = torch.distributed.get_rank()
+            num_workers = dataset_opt['n_workers']
+            assert dataset_opt['batch_size'] % world_size == 0
+            batch_size = dataset_opt['batch_size'] // world_size
+            shuffle = False
+        else:
+            num_workers = dataset_opt['n_workers'] * len(opt['gpu_ids'])
+            batch_size = dataset_opt['batch_size']
+            shuffle = True
+        return torch.utils.data.DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            sampler=sampler,
+            drop_last=True,
+            pin_memory=False)
+    else:
+        return torch.utils.data.DataLoader(
+            dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+
+
+def create_dataset(dataset_opt):
+    mode = dataset_opt['mode']
+    if mode == 'REDS':
+        from data.REDS_dataset import REDSDataset as D
+    else:
+        raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
+    dataset = D(dataset_opt)
+
+    logger = logging.getLogger('base')
+    logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
+                                                           dataset_opt['name']))
+    return dataset
