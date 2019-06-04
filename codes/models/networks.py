845ce3b0868171c8b4a7b82736f7d17ae0f5b486
XintaoWang <wxt1994@126.com>
2019-06-04 21:38:48 +0800

    add networks

---
+++ codes/models/networks.py
@@ -0,0 +1,25 @@
+import models.modules.EDVR_arch as EDVR_arch
+
+
+####################
+# define network
+####################
+# Generator
+def define_G(opt):
+    opt_net = opt['network_G']
+    which_model = opt_net['which_model_G']
+
+    if which_model == 'EDVR':
+        netG = EDVR_arch.EDVR(
+            nf=opt_net['nf'],
+            nframes=opt_net['nframes'],
+            groups=opt_net['groups'],
+            front_RBs=opt_net['front_RBs'],
+            back_RBs=opt_net['back_RBs'],
+            center=opt_net['center'],
+            predeblur=opt_net['predeblur'],
+            HR_in=opt_net['HR_in'])
+    else:
+        raise NotImplementedError('Generator model [{:s}] not recognized'.format(which_model))
+
+    return netG
